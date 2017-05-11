ddf1329c7b57bbbca8ce0cc9fc85356ae0682343
abdul-rehman-yousaf <abdul12391@gmail.com>
2017-05-11 23:29:28 +0500

    A New version of Instew App.

---
+++ groups/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.db.models import Q
+from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+
+from . import models
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        fields = ('name', 'description')
+        model = models.Company
+
+
+class FamilyForm(forms.ModelForm):
+    class Meta:
+        fields = ('name', 'description')
+        model = models.Family
+
+
+class CompanyInviteForm(forms.Form):
+    email_or_username = forms.CharField(label="Email/Username")
+
+    def clean_email_or_username(self):
+        data = self.cleaned_data['email_or_username']
+        try:
+            self.invitee = models.User.objects.get(
+                Q(email=data)|Q(username=data)
+            )
+        except models.User.DoesNotExist:
+            raise ValidationError('No such user')
+        return data
+
+
+class FamilyInviteForm(forms.Form):
+    email_or_username = forms.CharField(label="Email/Username")
+
+    def clean_email_or_username(self):
+        data = self.cleaned_data['email_or_username']
+        try:
+            self.invitee = models.User.objects.get(
+                Q(email=data)|Q(username=data)
+            )
+        except models.User.DoesNotExist:
+            raise ValidationError('No such user')
+        return data
+
+class LeaveForm(forms.Form):
+    pass
