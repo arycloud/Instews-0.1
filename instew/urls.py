1aa91ef8e9ea2ad4d71a526654e16c4c5c2640e0
abdul-rehman-yousaf <abdul12391@gmail.com>
2017-05-19 08:31:57 +0500

    Setup Users & Thoughts basic Api endpoints.

--- instew/urls.py
+++ instew/urls.py
@@ -24,6 +24,13 @@ from users import urls as user_urls
 from groups import urls as group_urls
 
 from users.serializers import router as user_router
+from thoughts import routers as thought_router
+
+api_urlpatterns = [
+    url(r'', include(user_router.urls)),
+    url(r'', include(thought_router.router.urls))
+]
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -31,12 +38,10 @@ urlpatterns = [
     url(r'^thoughts/', include(thought_urls, namespace='thoughts')),
     url(r'^groups/', include(group_urls, namespace='groups')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-<<<<<<< HEAD
+
     url(r'^(?P<username>\w+)/', include('users.urls')),
-=======
-    url(r'^api/', include(user_router.urls, namespace='rest_framework')),
+    url(r'^api/', include(api_urlpatterns)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
->>>>>>> Instews-Api
 ]
 
 if settings.DEBUG:
