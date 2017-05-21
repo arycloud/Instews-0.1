ea3119c4f10c085e8291ee175da437481120fe11
Abdul Rehman <Abdul-Rehman-Yousaf>
2017-05-21 20:33:53 +0500

    Setup Serializers,Routers & Viewsets for Users.

--- users/routers.py
+++ users/routers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 
-from rest_framework import routers,serializers, viewsets
+from rest_framework import routers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
---
+++ users/viewsets.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+
+from . import serializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
